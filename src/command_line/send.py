a31c81ec60d5e001be52747fa6e09c366c6d5382
Redfourk <turnej2096@g.comalisd.org>
2026-02-17 19:38:48 -0600

    initial

--- src/command_line/send.py
+++ src/command_line/send.py
@@ -2,9 +2,9 @@ import socket
 
 # Connect
 s = socket.socket()
-s.connect(('172.31.11.167', 5001))
+s.connect(('0.0.0.0', 5001))
 
 # Read and Send
-with open('my_data.zip', 'rb') as f:
+with open('layout.tcss', 'rb') as f:
     s.sendall(f.read())
 s.close()
