41b9fa8b7c99e93c6a6174fc49b9a49a07765d96
Redfourk <turnej2096@g.comalisd.org>
2026-03-16 10:29:28 -0500

    Dev 3.16.26.10.29

--- cli.py
+++ cli.py
@@ -1,3 +1,8 @@
+import logging
+
+
+import time
+
 print("Welcome to the FileLauncher CLI")
 print("Please take some time to input some settings.")
 
@@ -5,6 +10,7 @@ print("Please take some time to input some settings.")
 def verbose_logs():
     show_log = input("Would you like to see the logs? (Y/N): ")
     if show_log == "Y" or show_log == "y":
+        logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
         return True
     elif show_log == "N" or show_log == "n":
         return False
@@ -13,6 +19,9 @@ def verbose_logs():
 vblogs = verbose_logs()
 
 # Check for Network:
+if vblogs == bool(True):
+    logging.debug('Identifying network')
+
 from netman_cli import *
 def netcheck():
     print("A network is needed to use this program and the current status on this device has been identified as " + check_adapter_status())
@@ -21,8 +30,7 @@ def netcheck():
         return True
     elif netcorrect == "N" or netcorrect == "n":
         print("Please connect to a network or check out the repo for more information")
-        input("Press Enter to exit...")
-        return False
+        exit("Network Error")
     else:
         netcheck()
 net_status = netcheck()
@@ -39,6 +47,9 @@ def role():
         role()
 role_type = role()
 
+if vblogs == bool(True):
+    logging.debug("Role set to " + role_type)
+time.sleep(1)
 
 # Show Selected Settings:
 print(
