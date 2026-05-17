fc6bf928338f232892f08e54a5a488090883f91e
Redfourk <turnej2096@g.comalisd.org>
2026-05-17 18:02:08 -0500

    Dev 05.15.26.15.21

--- src/updates/check_repo.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def get_latest_version(repo_owner, repo_name):
-    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases/latest"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()["tag_name"]
-    return None
-
